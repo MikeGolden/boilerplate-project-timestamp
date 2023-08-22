e67af71caa582ad46b19544c7931e34a4f28cbc1
Mike Goldenberg <csrutil@tuta.io>
2023-08-22 19:41:33 +0200

    first attempt to build time endpoint

--- index.js
+++ index.js
@@ -24,6 +24,43 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/timestamp/:date", (req, res) => {
+  let dateString = req.params.date_string;
+
+  //A 4 digit number is a valid ISO-8601 for the beginning of that year
+  //5 digits or more must be a unix time, until we reach a year 10,000 problem
+  if (/\d{5,}/.test(dateString)) {
+    let dateInt = parseInt(dateString);
+    //Date regards numbers as unix timestamps, strings are processed differently
+    res.json({ unix: dateString, utc: new Date(dateInt).toUTCString() });
+  } else {
+    let dateObject = new Date(dateString);
+
+    if (dateObject.toString() === "Invalid Date") {
+      res.json({ error: "Invalid Date" });
+    } else {
+      res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+    }
+  }
+});
+
+app.get('/api/:date', (req, res) => {
+  const inputDate = req.params.date;
+
+  if (inputDate) {
+    const timestamp = Date.parse(inputDate);
+    
+    if (!isNaN(timestamp)) {
+      const unixTimestamp = new Date(inputDate).getTime();
+      res.json({ unix: unixTimestamp });
+    } else {
+      res.status(400).json({ error: 'Invalid date format' });
+    }
+  } else {
+    const currentUnixTimestamp = new Date().getTime();
+    res.json({ unix: currentUnixTimestamp });
+  }
+});
 
 
 // listen for requests :)
